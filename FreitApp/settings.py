844e8d657bb487ba58831aa2550f847a8cbfce58
Ejike Sylva <sylvaejike@gmail.com>
2021-12-16 22:25:50 +0100

    lifetime config

--- FreitApp/settings.py
+++ FreitApp/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     'drf_yasg',
     'order',
     'driver',
-
 ]
 
 MIDDLEWARE = [
@@ -108,7 +107,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1)
 }
 
